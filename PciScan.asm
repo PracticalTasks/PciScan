c5914fb1708cabbf8d7f45b564ebca492ca45e88
Alexandr <kajlaleksandr1986@gmail.com>
2021-04-30 12:22:22 +0700

    add source file

---
+++ PciScan.asm
@@ -0,0 +1,247 @@
+;PciRecScan.asm
+;Created by Alexandr Kail
+;Fasm, cp1251
+
+;яйюмхпсхл ьхмс PCI
+;0-7 хмдейя пхцхярпю
+;8-10 тсмйжхъ
+;11-15 сярпниярбю
+;16-23 ьхмю
+;24-30 пхгепб 0
+;31 C - ткюц днярсою й сярпниярбс 1 	
+Start:
+		MOV		AX,CS
+		MOV		DS,AX
+			CLI
+		MOV		SS,AX
+		MOV		SP,Start
+			STI
+;!!!!!!!!!!!!!сярюмнбхрэ бхден пефхл 
+		MOV		AH,1
+		MOV		CX,2000H
+	INT		10H								;сахпюел йспянп
+		MOV		AX,0B800H               	
+		MOV		ES,AX						;сярнмюбкебюел яецлемр мю бхден оюлърэ
+		XOR		DI,DI						;сйюгюрекэ б бхденоюлърх
+;пюглевюел онкъ	
+	CALL	FieldMarkup
+		XOR		CL,CL
+
+;жхйк яйюмхпнбюмхъ сярпниярб		
+CheckDevice:		 
+;оюксвюел дюммше хг оюпрнб	I/O					
+		MOV		EAX,[confAddr]				;мювюкэмши юдпея(BUS_0,DEV_0,FUN_0,_REG_0),ярюпьхи ахр гюпегепбхпнбюм
+		MOV		DX,0CF8H                    
+		OUT		DX,EAX						;бшярнбкъхл юдпея б онпр PCI CONFIG_ADDRESS	
+		MOV		DX,0CFCH	                
+		IN		EAX,DX						;вхрюел пецхярп йнмт. опнярпюмярбю PCI хг CONFIG_DATA
+		CMP		AX,0FFFFH					;еякх VenID = 0FFFFH, сярпниярбн нрясрярбсер.
+	JE	.End								;бшахпюел якедсчыее сярпниярбн
+		PUSH	EAX							;янупнмъел пецхярп б ярщй
+		
+		MOV		EAX,[confAddr]				
+		ADD		EAX,08H						;сярюмюбкхбюел хмдейя пецхярпю йнмт. опнярюм PCI
+		MOV		DX,0CF8H
+		OUT		DX,EAX						
+		MOV		DX,0CFCH
+		IN		EAX,DX	
+		MOV		[regClCode],EAX	
+		
+		MOV		EAX,[confAddr]				
+		ADD		EAX,0CH						;сярюмюбкхбюел хмдейя пецхярпю йнмт. опнярюм PCI
+		MOV		DX,0CF8H
+		OUT		DX,EAX						
+		MOV		DX,0CFCH
+		IN		EAX,DX	
+		MOV		[regHeadType],EAX		
+		
+;бшбндхл дюммше мю щйпюм					
+		MOV		DL,BYTE[confAddr+2]			;бшъямъел мнлеп ьхмш
+		ADD		[addrBdf],160				;якедсчыюъ ярпнйю бшбндю б бхденоюлърх(80 * (мю яхлб. + юрпха.))	
+		MOV		DI,[addrBdf]	
+		CMP		DI,0F00H					;онякедмъъ кх ярпнйю?	
+	JE	NextPage							;сярюмюбкхбюел бшбнд мю мнбши щйпюм
+.L1:
+	CALL	ConvNumOfStr1					;бшбндел мнлеп ьхмш
+		MOV		AL,'_'				           
+		STOSW								;бшбндел мхфмхи якщь
+		MOV		DL,BYTE[confAddr+1]			   		
+		SHR		DL,3						;мнлеп сярпниярбю ярюпьхе 5 ахр
+	CALL	ConvNumOfStr1
+		MOV		AL,'_'
+		STOSW
+		MOV		DL,BYTE[confAddr+1]
+		AND		DL,7						;яювйхпсел ярюпьхе 5 ахр; мнлеп тсмйжхх 	
+	CALL	ConvNumOfStr1	                
+		POP		EBX							;б EBX пецхярп йнмт. опнярпюмярбю PCI
+		ADD		[addrVen],160				;онйюгшбюер мю леярн дкъ бшбндю VenID
+		MOV		DI,[addrVen]
+	CALL	ConvNumOfStr2
+		SHR		EBX,16
+		ADD		[addrDev],160				;онйюгшбюер мю леярн дкъ бшбндю DevID
+		MOV		DI,[addrDev]				
+	CALL	ConvNumOfStr2	
+
+		MOV		BX,WORD[regClCode+2]
+		ADD		[addrRegPci],160				;онйюгшбюер мю леярн дкъ бшбндю DevID
+		MOV		DI,[addrRegPci]				
+	CALL	ConvNumOfStr2	
+	
+		MOV		DL,BYTE[regHeadType+2]
+		ADD		[addrHeadType],160			;онйюгшбюер мю леярн бшапнммнцн RegPci
+		MOV		DI,[addrHeadType]
+	CALL	ConvNumOfStr1
+
+;бшахпюел якедсчыее сярпниярбн
+.End:	
+		CMP	 	[confAddr],80FFFF00H			;яйюмхпсел ндмс ьхмс
+	JE	_End
+		ADD		[confAddr],100H					;якедсчыее сярпниярбн, ьхмю
+	JMP	CheckDevice
+	
+_End:
+	JMP	$
+	
+;вей тсмйжхъ нопедекъчыюъ лняр PCI-PCI	
+
+	
+;тсмйжхъ ондцюрнбйх якедсчыеи ярпюмхжш	
+NextPage:
+		MOV		SI,msg
+		MOV		AH,00000111B	;аекши яхлбнк мю в╗пмюл тнме			
+	CALL	LineOut				;бшбндел яннаыемхе
+		MOV		AH,10H          
+	INT	16H						;явхршбюмхъ яхлбнкю я нфхдюмхел
+		XOR		EAX,EAX
+		XOR		DI,DI
+		MOV		CX,0FA0H
+	REP	STOSD
+		MOV		[addrBdf],160
+		MOV		[addrVen],32
+		MOV		[addrDev],64
+		MOV		[addrRegPci],96
+		MOV		[addrHeadType],128
+		XOR		DI,DI
+;пюглхвюхл оюкъ	
+	CALL	FieldMarkup
+		MOV		DI,160
+	JMP	CheckDevice.L1
+
+;пюглерйю онкъ	
+FieldMarkup:
+		MOV		SI,msgBdf				;бшбндхлюъ ярпнйю
+;гюлемхрэ мю йнмярюмрс
+		MOV		AH,00000100B			;йпюямши яхлбнк мю в╗пмнл тнме		
+	CALL	LineOut                       
+		MOV		DI,[addrVen]					;мюдохяэ VenID
+		MOV		SI,msgVen               
+		MOV		AH,00000100B			;йпюямши яхлбнк мю в╗пмнл тнме		
+	CALL	LineOut				        
+		MOV		DI,[addrDev]					;мюдохяэ DevID
+		MOV		SI,msgDev               
+		MOV		AH,00000100B			;йпюямши яхлбнк мю в╗пмнл тнме		
+	CALL	LineOut
+		MOV		DI,[addrRegPci]
+		MOV		SI,msgClassCode
+		MOV		AH,00000100B
+	CALL	LineOut
+		MOV		DI,[addrHeadType]
+		MOV		SI,msgHeadType	
+		MOV		AH,00000100B
+	CALL	LineOut
+	
+	RET	
+		
+;опенапюгсел дбю аюирю б ASCII яхлбнк
+;б BX бшбндхлне HEX гмювемхе
+;б ES:DI юдпея бхденоюлърх
+ConvNumOfStr2:		
+		MOV		AH,00000100B	
+		MOV		AL,BH			;ярюпьхи аюир
+;!!!!!!!!!!!!!! оПХДСЛЮРЭ МЮГБЮМХЕ
+	CALL	Fun1				;бшбндхл аюир
+		MOV		AL,BL			;лкюдьхи аюир
+	CALL	Fun1                
+
+	RET                         
+Fun1:	                        
+		PUSH	AX				;янупюмъел AL
+		SHR		AL,4			;ярюпьхи пюгпъд аюирю
+	CALL 	CharOut				;бшбнд яхлбнкю
+		POP		AX				;бнярюмюбкебюел
+		AND		AL,00001111B	;лкюдьхи пюгпъд аюирю		
+	CALL	CharOut			    
+
+	RET                         
+
+CharOut:						
+		CMP		AL,0AH			;жхтпш хкх асйбш
+		JAE	L1	                
+		ADD		AL,30H			;опеюапюгсхл б ASCII
+@@:			                    
+		STOSW                   
+	RET                         
+
+L1:                             
+		ADD		AL,37H			;опеюапюгсхл б ASCII
+	JMP	@b	
+
+;опенапюгсел 1 аюир б ASCII яхлбнк
+;б DL бшбндхлне HEX гмювемхе
+;б ES:DI юдпея бхденоюлърх
+ConvNumOfStr1:
+		MOV		AH,00000100B	
+		MOV		AL,DL			;бшбндхлши аюир	
+		PUSH	AX				;янупюмъел AL
+		SHR		AL,4			;ярюпьхи пюгпъд аюирю
+	CALL 	.CharOut			;бшбнд яхлбнкю
+		POP		AX				;бнярюмюбкебюел
+		AND		AL,00001111B	;лкюдьхи пюгпъд аюирю
+	CALL	.CharOut							
+	RET                         
+
+.CharOut:                       
+		CMP		AL,0AH			;жхтпш хкх асйбш
+		JAE	.L1	                
+		ADD		AL,30H			;опенапюгсел б ASCII
+@@:			                    
+		STOSW                   
+	RET                         
+	
+.L1:                            
+		ADD		AL,37H			;опенапюгсел б ASCII
+	JMP	@b	
+	
+;бшбндел ярпнйс яхлбнкнб гюйюмвхбючысчяъ 0
+;б DS:SI ярпнйю яхлбнкнб	
+;б ES:DI ялеыемхе б бхденоюлърх
+;б AH юрпхаср яхлбнкю		
+LineOut:						
+		MOV		AL,[SI]
+		CMP		AL,0
+	JE	.End
+			STOSW	
+		INC		SI
+	JMP	LineOut
+	
+.End:	
+	RET	
+	
+
+
+;==========================дюммшх==================================
+msgBdf			DB	'B_D_F',0					;ьхмю_сярп_тсм
+msgVen			DB	'VenID',0
+msgDev			DB	'DevID',0
+msgClassCode	DB	'ClCode/SubCl',0
+msgHeadType		DB	'HeaderType',0
+msg				DB	"Press any button to continue...",0
+;0-2 тсмйжхъ(3 ахрю),3-7 сярпниярбн(5 ахр),8-15 ьхмю(8 ахр)
+confAddr		DD	80000000H					;мювюкэмши юдпея
+regClCode		DD  0	
+regHeadType		DD	0							;оепелеммюъ дкъ упюмемхъ пец. PCI	
+addrBdf			DW	0							;леярн бшбндю "B_D_F"
+addrVen			DW	32							;леярн бшбндю VenID	
+addrDev			DW	64							;леярн бшбндю DevID		
+addrRegPci		DW	96							;леярн бшбндю пец. PCI
+addrHeadType	DW	128							
